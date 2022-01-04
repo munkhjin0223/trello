1feb5ac4b727898cf29a2873a9cd27560e1f76e0
Munkhjin Badamkhatan <munkhjin0223@gmail.com>
2022-01-04 19:06:28 +0800

    initial

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import { render } from 'react-dom';
+
+const App = () => {
+  return <div>Trello like</div>;
+};
+
+const rootElement = document.getElementById('root');
+render(<App />, rootElement);
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html')
+    })
+  ]
+};
