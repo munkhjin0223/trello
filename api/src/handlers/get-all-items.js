5dc4abbdd2cff220d97d9b266f2255ddc6556a4a
Munkhjin Badamkhatan <munkhjin0223@gmail.com>
2022-01-23 20:20:51 +0800

    add step to deploy serverless app for workflow

--- api/src/handlers/get-all-items.js
+++ api/src/handlers/get-all-items.js
@@ -10,28 +10,32 @@ const docClient = new dynamodb.DocumentClient();
 /**
  * A simple example includes a HTTP get method to get all items from a DynamoDB table.
  */
-exports.getAllItemsHandler = async (event) => {
-    if (event.httpMethod !== 'GET') {
-        throw new Error(`getAllItems only accept GET method, you tried: ${event.httpMethod}`);
-    }
-    // All log statements are written to CloudWatch
-    console.info('received:', event);
+exports.getAllItemsHandler = async event => {
+  if (event.httpMethod !== 'GET') {
+    throw new Error(
+      `getAllItems only accept GET method, you tried: ${event.httpMethod}`
+    );
+  }
+  // All log statements are written to CloudWatch
+  console.info('received:', event);
 
-    // get all items from the table (only first 1MB data, you can use `LastEvaluatedKey` to get the rest of data)
-    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#scan-property
-    // https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_Scan.html
-    var params = {
-        TableName : tableName
-    };
-    const data = await docClient.scan(params).promise();
-    const items = data.Items;
+  // get all items from the table (only first 1MB data, you can use `LastEvaluatedKey` to get the rest of data)
+  // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#scan-property
+  // https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_Scan.html
+  var params = {
+    TableName: tableName
+  };
+  const data = await docClient.scan(params).promise();
+  const items = data.Items;
 
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(items)
-    };
+  const response = {
+    statusCode: 200,
+    body: `List: ${JSON.stringify(items)}`
+  };
 
-    // All log statements are written to CloudWatch
-    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
-    return response;
-}
+  // All log statements are written to CloudWatch
+  console.info(
+    `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
+  );
+  return response;
+};
